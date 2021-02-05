82af3f28ae957fd1ef86e99e08df9aaa63cd3c5c
Victorguz <victorguzber@gmail.com>
2021-02-05 03:24:26 -0500

    home and firebase

--- coky-frontend/src/app/view/public/public-prices/public-prices.component.ts
+++ coky-frontend/src/app/view/public/public-prices/public-prices.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-public-prices',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./public-prices.component.scss']
 })
 export class PublicPricesComponent implements OnInit {
+
+  @Input()
+  home = false;
 
   constructor() { }
 
--- coky-frontend/src/app/view/public/public-services/public-services.component.ts
+++ coky-frontend/src/app/view/public/public-services/public-services.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-public-services',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./public-services.component.scss']
 })
 export class PublicServicesComponent implements OnInit {
+
+  @Input()
+  home = false;
 
   constructor() { }
 
